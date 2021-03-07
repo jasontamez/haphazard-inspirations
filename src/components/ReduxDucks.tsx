26c6130f7ed357b12b6ab6b59ef3d41f1acd04f2
Jason Tamez <jasontamez@users.noreply.github.com>
2021-03-07 03:30:46 -0600

    *mods for theme, laying foundations

--- src/components/ReduxDucks.tsx
+++ src/components/ReduxDucks.tsx
@@ -1,10 +1,27 @@
+import maybeUpdateTheme from './MaybeUpdateTheme';
+
+const p = "haphazard-inspiration/reducer/";
+const UPDATE_THEME = p+"UPDATE_THEME";
+
+export function updateTheme(payload: string) {
+	return {type: UPDATE_THEME, payload};
+}
 
 interface StateObject {
-
+	theme: string
 }
-const initialState: StateObject = {};
-export const blankAppState: StateObject = {};
+export const blankAppState: StateObject = {
+	theme: "Default"
+};
 
-export function reducer(state: StateObject = initialState, action: any) {
-	return {};
+export function reducer(state: StateObject = blankAppState, action: any) {
+	const payload = action.payload;
+	let final: StateObject = {...state};
+	switch(action.type) {
+		case UPDATE_THEME:
+			final.theme = payload;
+			maybeUpdateTheme(state.theme, final.theme);
+			break;
+	}
+	return final;
 }
