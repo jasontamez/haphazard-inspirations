685db8d93da8b561f30930931b457f3490242d4f
Jason Tamez <jasontamez@users.noreply.github.com>
2021-03-08 02:06:33 -0600

    +comments, saving state, debounce

--- src/components/ReduxDucks.tsx
+++ src/components/ReduxDucks.tsx
@@ -1,12 +1,33 @@
 import maybeUpdateTheme from './MaybeUpdateTheme';
+import { StateStorage } from './PersistentInfo';
+import debounce from './Debounce';
 
+//
+//
+// CONSTS
+//
+//
 const p = "haphazard-inspiration/reducer/";
 const UPDATE_THEME = p+"UPDATE_THEME";
 
+//
+//
+// FUNCTIONS
+//
+//
 export function updateTheme(payload: string) {
 	return {type: UPDATE_THEME, payload};
 }
 
+//
+//
+// INTERFACES
+//
+//
+interface ReducerAction {
+	type: string
+	payload?: any
+}
 interface StateObject {
 	theme: string
 }
@@ -14,14 +35,34 @@ export const blankAppState: StateObject = {
 	theme: "Default"
 };
 
-export function reducer(state: StateObject = blankAppState, action: any) {
+//
+//
+// SUB-REDUCERS
+//
+//
+
+//
+//
+// REDUCER
+//
+//
+export function reducer(state: StateObject = blankAppState, action: ReducerAction) {
 	const payload = action.payload;
 	let final: StateObject = {...state};
 	switch(action.type) {
 		case UPDATE_THEME:
 			final.theme = payload;
-			maybeUpdateTheme(state.theme, final.theme);
+			maybeUpdateTheme(state.theme, payload);
 			break;
+		default:
+			return state;
 	}
+	debounce(saveCurrentState, [final]);
 	return final;
 }
+
+const saveCurrentState = (state: StateObject) => {
+	let newState = {...state};
+	StateStorage.setItem("lastState", newState);
+};
+
