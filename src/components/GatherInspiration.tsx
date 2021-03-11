d502b9a092c4f392d4cdf36a6b2100e5de2ba2e1
Jason Tamez <jasontamez@users.noreply.github.com>
2021-03-11 00:12:42 -0600

    finishing this up

--- src/components/GatherInspiration.tsx
+++ src/components/GatherInspiration.tsx
@@ -7,9 +7,36 @@ import object from '../data/characters.json';
 import topic from '../data/characters.json';
 
 //npm install english-number
+//import { nameOf } from 'english-number';
+//const englishNumber = (input: number) => nameOf(input).toLowerCase().replace(/ and /g, " ");
+//export default englishNumber;
 
-interface BasicIdea {
-	name: string,
+const EnglishNumbers = [
+	"zero",
+	"one",
+	"two",
+	"three",
+	"four",
+	"five",
+	"six",
+	"seven",
+	"eight",
+	"nine",
+	"ten",
+	"eleven",
+	"twelve",
+	"thirteen",
+	"fourteen",
+	"fifteen",
+	"sixteen",
+	"seventeen",
+	"eighteen",
+	"nineteen",
+	"twenty"
+];
+
+class BasicIdea {
+	name?: string
 	fantasy?: boolean
 	medievalFantasy?: boolean
 	historicalFiction?: boolean
@@ -33,26 +60,114 @@ interface BasicIdea {
 	alcohol?: boolean
 	tobacco?: boolean
 	modern?: boolean
+	constructor(initializer: any) {
+		let newObj = this;
+		Object.getOwnPropertyNames(initializer).forEach((prop) =>(newObj[prop as keyof BasicIdea] = initializer[prop]));
+	}
 }
 
 // Character, Object
-interface PossiblePlural extends BasicIdea {
-	min: number
-	max: number
-	rateBy: number
-	rateInverse: boolean
-	plural: string | boolean
-	article: string
-	numeric: boolean
+class PossiblePlural extends BasicIdea {
+	min?: number
+	max?: number
+	rateBy?: number | "incremental"
+	rateFavorsLower?: boolean
+	plural?: string | boolean
+	article?: string
+	numerals?: boolean
+	constructor(initializer: any) {
+		super(initializer);
+	}
+	getNumber(n: number) {
+		return EnglishNumbers[n];
+	}
+}
+class AnObject extends PossiblePlural {
+	constructor(initializer: any) {
+		super(initializer);
+	}
+}
+let proto: any = AnObject.prototype;
+let def: any = object.default;
+proto.min = def.min;
+proto.max = def.max;
+proto.rateBy = def.rateBy;
+proto.rateFavorsLower = def.rateFavorsLower;
+proto.plural = def.plural;
+proto.article = def.article;
+proto.numerals = def.numerals;
+
+class Character extends PossiblePlural {
+	constructor(initializer: any) {
+		super(initializer);
+	}
+}
+proto = Character.prototype;
+def = characters.default;
+proto.min = def.min;
+proto.max = def.max;
+proto.rateBy = def.rateBy;
+proto.rateFavorsLower = def.rateFavorsLower;
+proto.plural = def.plural;
+proto.article = def.article;
+proto.numerals = def.numerals;
+
+class Locale extends BasicIdea {
+	place?: boolean
+	political?: boolean
+	geographical?: boolean
+	construct?: boolean
+	preposition?: string
+	size?: "large" | "medium" | "small" | "variable" | "tiny"
+	specific?: boolean
+	constructor(initializer: any) {
+		super(initializer);
+	}
+}
+proto = Locale.prototype;
+def = locale.default;
+proto.size = def.size;
+proto.specific = def.specific;
+proto.preposition = def.preposition;
+
+class AnEvent extends BasicIdea {
+	plural?: boolean
+	punctual?: boolean
+	constructor(initializer: any) {
+		super(initializer);
+	}
+}
+proto = AnEvent.prototype;
+def = event.default;
+proto.plural = def.plural;
+proto.punctual = def.punctual;
+
+class Topic extends BasicIdea {
+	constructor(initializer: any) {
+		super(initializer);
+	}
 }
 
-interface Locale extends BasicIdea {
-	type: "place" | "political" | "geographical" | "construct"
-	size: "large" | "medium" | "small" | "variable" | "tiny"
-	specific: boolean
+class Time extends BasicIdea {
+	constructor(initializer: any) {
+		super(initializer);
+	}
 }
 
-interface Event extends BasicIdea {
-	plural: boolean
-	punctual: boolean
+class Action extends BasicIdea {
+	constructor(initializer: any) {
+		super(initializer);
+	}
 }
+
+const inspirations = {
+	actions: action.content.map(a => new Action(a)),
+	characters: characters.content.map(c => new Character(c)),
+	events: event.content.map(e => new AnEvent(e)),
+	locales: locale.content.map(l => new Locale(l)),
+	objects: object.content.map(o => new AnObject(o)),
+	times: time.content.map(t => new Time(t)),
+	topics: topic.content.map(t => new Topic(t))
+};
+
+export default inspirations;
