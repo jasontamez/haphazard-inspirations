8be22a0e1cfa644f861473d34fd9fa2767f7feb0
Jason Tamez <jasontamez@users.noreply.github.com>
2021-03-27 23:47:21 -0500

    *update home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,41 +1,29 @@
 import React from 'react';
 import {
+	IonButtons,
 	IonContent,
 	IonHeader,
+	IonMenuButton,
 	IonPage,
 	IonTitle,
 	IonToolbar,
-	IonButton,
-	IonButtons
 } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import { useDispatch } from "react-redux";
-import { updateTheme } from '../components/ReduxDucks';
 import './Home.css';
 
 const Home: React.FC = () => {
-	const dispatch = useDispatch();
+//	const dispatch = useDispatch();
 	return (
 		<IonPage>
 			<IonHeader>
 				<IonToolbar>
 					<IonTitle>Inspire</IonTitle>
-					<IonButtons slot="end">
-						<IonButton onClick={() => dispatch(updateTheme("Default"))}>D</IonButton>
-						<IonButton onClick={() => dispatch(updateTheme("Light"))}>L</IonButton>
-						<IonButton onClick={() => dispatch(updateTheme("SolarizedDark"))}>SD</IonButton>
-						<IonButton onClick={() => dispatch(updateTheme("SolarizedLight"))}>SL</IonButton>
-						<IonButton onClick={() => dispatch(updateTheme("DeepPurple"))}>P</IonButton>
-						<IonButton onClick={() => dispatch(updateTheme("RoseGlasses"))}>R</IonButton>
+					<IonButtons slot="start">
+						<IonMenuButton />
 					</IonButtons>
 				</IonToolbar>
 			</IonHeader>
 			<IonContent fullscreen>
-				<IonHeader collapse="condense">
-					<IonToolbar>
-						<IonTitle size="large">Blank</IonTitle>
-					</IonToolbar>
-				</IonHeader>
 				<ExploreContainer />
 			</IonContent>
 		</IonPage>
