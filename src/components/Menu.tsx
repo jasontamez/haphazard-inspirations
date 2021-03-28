9227d78a76ed12166cf9d261d788425e5732c0f7
Jason Tamez <jasontamez@users.noreply.github.com>
2021-03-27 23:46:48 -0500

    *setting up the menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -7,152 +7,57 @@ import {
 	IonListHeader,
 	IonMenu,
 	IonNote,
+	IonMenuToggle
 } from '@ionic/react';
 import React from 'react';
-import { useLocation } from 'react-router-dom';
 import {
-	shuffleSharp,
-	bookSharp,
 	settingsSharp,
 	chatboxEllipsesSharp,
-	caretForwardSharp,
-	ellipseSharp
+	colorPaletteSharp,
+	homeSharp
 } from 'ionicons/icons';
 import './Menu.css';
 
-interface AppPage {
-	url: string
-	title: string
-	icon?: string
-	id: string
-	parent?: string
-	parentOf? : string
-}
-interface MenuSection {
-	header?: string
-	note?: string
-	pages: AppPage[]
-	id: string
-}
-
-const appMenuPages: MenuSection[] = [
-	{
-		header: "Conlang Toolbox",
-		note: "tools for conlangers",
-		pages: [
-			{
-				title: 'WordEvolve',
-				url: '/we',
-				icon: shuffleSharp,
-				id: 'menuitemWE',
-				parentOf: 'we'
-			},
-			{
-				title: 'Input',
-				url: '/we/input',
-				id: 'menuitemWEinp',
-				parent: 'we'
-			},
-			{
-				title: 'Categories',
-				url: '/we/categories',
-				id: 'menuitemWEcat',
-				parent: 'we'
-			},
-			{
-				title: 'Transformations',
-				url: '/we/transformations',
-				id: 'menuitemWEtns',
-				parent: 'we'
-			},
-			{
-				title: 'Sound Changes',
-				url: '/we/soundchanges',
-				id: 'menuitemWEscs',
-				parent: 'we'
-			},
-			{
-				title: 'Output',
-				url: '/we/output',
-				id: 'menuitemWEout',
-				parent: 'we'
-			},
-			{
-				title: 'Lexicon',
-				url: '/lex',
-				icon: bookSharp,
-				id: 'menuitemLX'
-			}
-		],
-		id: 'menuMain'
-	},
-	{
-		pages: [
-			{
-				title: 'Settings',
-				url: '/settings',
-				icon: settingsSharp,
-				id: 'menuitemSettings'
-			},
-			{
-				title: 'About',
-				url: '/about',
-				icon: chatboxEllipsesSharp,
-				id: 'menuitemAbout'
-			}
-		],
-		id: 'menuOthers'
-	}
-];
-
 const Menu = () => {
-	const location = useLocation();
-
 	return (
 		<IonMenu contentId="main" type="overlay" id="mainMenu">
 			<IonContent>
-				{appMenuPages.map((menuSection) => {
-					const pages = menuSection.pages.map((appPage) => {
-						if(appPage.parentOf) {
-							return (
-								<IonItem
-									key={appPage.id}
-									className={
-										'mainHeading'
-										+ (location.pathname.startsWith(appPage.url) ? ' selected' : '')
-									}
-									lines="none"
-									detail={false}
-								>
-									<IonIcon slot="start" icon={appPage.icon} />
-									<IonLabel>{appPage.title}</IonLabel>
-									<IonIcon className="caret" slot="end" icon={caretForwardSharp} />
-								</IonItem>
-							);
-						} else if(appPage.parent) {
-							return (
-								<IonItem key={appPage.id} className={'subHeading' + (location.pathname.startsWith(appPage.url) ? ' selected' : '')} routerLink={appPage.url} routerDirection="forward" lines="none" detail={false}>
-									<IonLabel>{appPage.title}</IonLabel>
-									<IonIcon slot="end" size="small" icon={ellipseSharp} />
-								</IonItem>
-							);
-						}
-						return (
-							<IonItem key={appPage.id} className={'mainHeading' + (location.pathname.startsWith(appPage.url) ? ' selected' : '')} routerLink={appPage.url} routerDirection="forward" lines="none" detail={false}>
-								<IonIcon slot="start" icon={appPage.icon} />
-								<IonLabel>{appPage.title}</IonLabel>
-							</IonItem>
-						);
-				});
-					let head: any = (menuSection.header) ? (<IonListHeader>{menuSection.header}</IonListHeader>) : '',
-						note: any = (menuSection.note) ? (<IonNote>{menuSection.note}</IonNote>) : '';
-					return (
-						<IonList key={menuSection.id}>
-							{head}{note}
-							{pages}
-						</IonList>
-					);
-				})}
+				<IonList lines="none">
+					<IonListHeader>Haphazard Inspirations</IonListHeader>
+					<IonNote></IonNote>
+					<IonMenuToggle autoHide={false}>
+						<IonItem routerLink="/settings" routerDirection="forward" detail={false}>
+							<IonIcon slot="start" icon={homeSharp} />
+							<IonLabel>Home</IonLabel>
+						</IonItem>
+					</IonMenuToggle>
+				</IonList>
+				<IonList lines="none">
+					<IonMenuToggle autoHide={false}>
+						<IonItem routerLink="/settings" routerDirection="forward" detail={false}>
+							<IonIcon slot="start" icon={settingsSharp} />
+							<IonLabel>Settings</IonLabel>
+						</IonItem>
+					</IonMenuToggle>
+					<IonMenuToggle autoHide={false}>
+						<IonItem routerLink="/about" routerDirection="forward" detail={false}>
+							<IonIcon slot="start" icon={chatboxEllipsesSharp} />
+							<IonLabel>About</IonLabel>
+					</IonItem>
+					</IonMenuToggle>
+					<IonMenuToggle autoHide={false}>
+						<IonItem routerLink="/theme" routerDirection="forward" detail={false}>
+							<IonIcon slot="start" icon={colorPaletteSharp} />
+							<IonLabel>Theme</IonLabel>
+						</IonItem>
+					</IonMenuToggle>
+					<IonMenuToggle autoHide={false}>
+						<IonItem routerLink="/settings" routerDirection="forward" detail={false}>
+							<IonIcon slot="start" icon={settingsSharp} />
+							<IonLabel></IonLabel>
+						</IonItem>
+					</IonMenuToggle>
+				</IonList>
 			</IonContent>
 		</IonMenu>
 	);
