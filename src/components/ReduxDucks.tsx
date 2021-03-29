2953c43d7cd7d34de8a487c389e51e7843a53576
Jason Tamez <jasontamez@users.noreply.github.com>
2021-03-29 01:10:42 -0500

    *Settings page got a LOT of new stuff

--- src/components/ReduxDucks.tsx
+++ src/components/ReduxDucks.tsx
@@ -9,7 +9,13 @@ import debounce from './Debounce';
 //
 const p = "haphazard-inspiration/reducer/";
 const UPDATE_THEME = p+"UPDATE_THEME";
-const TOGGLE_BOOL = p+"TOGGLE_BOOL";
+const TOGGLE_SETTING = p+"TOGGLE_SETTING";
+const TOGGLE_LOCALE = p+"TOGGLE_LOCALE";
+const TOGGLE_GENRE = p+"TOGGLE_GENRE";
+const TOGGLE_CONTENT = p+"TOGGLE_CONTENT";
+const TOGGLE_PERSON = p+"TOGGLE_PERSON";
+const TOGGLE_EVENT = p+"TOGGLE_EVENT";
+const TOGGLE_TRIGGER = p+"TOGGLE_TRIGGER";
 
 //
 //
@@ -19,8 +25,26 @@ const TOGGLE_BOOL = p+"TOGGLE_BOOL";
 export function updateTheme(payload: string) {
 	return {type: UPDATE_THEME, payload};
 }
-export function toogleBooleanOption(payload: boolProp) {
-	return {type: TOGGLE_BOOL, payload};
+export function toggleSetting(payload: keyof SettingsObject) {
+	return {type: TOGGLE_SETTING, payload};
+}
+export function toggleLocale(payload: keyof LocalesObject) {
+	return {type: TOGGLE_LOCALE, payload};
+}
+export function toggleGenre(payload: keyof GenresObject) {
+	return {type: TOGGLE_GENRE, payload};
+}
+export function toggleContent(payload: keyof ContentObject) {
+	return {type: TOGGLE_CONTENT, payload};
+}
+export function togglePerson(payload: keyof PersonObject) {
+	return {type: TOGGLE_PERSON, payload};
+}
+export function toggleEvent(payload: keyof EventObject) {
+	return {type: TOGGLE_EVENT, payload};
+}
+export function toggleTrigger(payload: keyof TriggersObject) {
+	return {type: TOGGLE_TRIGGER, payload};
 }
 
 //
@@ -32,14 +56,108 @@ interface ReducerAction {
 	type: string
 	payload?: any
 }
-interface StateObject {
-	theme: string
+interface SettingsObject {
 	shake: boolean
 }
-type boolProp = "shake";
+interface LocalesObject {
+	any: boolean
+	specific: boolean
+	political: boolean
+	large: boolean
+	medium: boolean
+	small: boolean
+	tiny: boolean
+}
+interface GenresObject {
+	fantasy: boolean
+	medievalFantasy: boolean
+	historicalFiction: boolean
+	western: boolean
+	samurai: boolean
+	horror: boolean
+	scifi: boolean
+	spacefaring: boolean
+}
+interface ContentObject {
+	properName: boolean
+	modern: boolean
+	religionAndMyths: boolean
+	christianity: boolean
+	greekRomanMyth: boolean
+	sexual: boolean
+	illicitSubstances: boolean
+	alcohol: boolean
+	tobacco: boolean
+}
+interface PersonObject {
+	realPerson: boolean
+}
+interface EventObject {
+	nonPunctualEvent: boolean
+}
+interface TriggersObject {
+	humanDeath: boolean
+	humanDistress: boolean
+	animalDeath: boolean
+	animalDistress: boolean
+}
+export interface StateObject {
+	theme: string
+	settings: SettingsObject
+	locales: LocalesObject
+	genres: GenresObject
+	content: ContentObject
+	person: PersonObject
+	event: EventObject
+	triggers: TriggersObject
+}
 export const blankAppState: StateObject = {
 	theme: "Default",
-	shake: false
+	settings: {
+		shake: false
+	},
+	locales: {
+		any: false,
+		specific: false,
+		political: false,
+		large: false,
+		medium: false,
+		small: false,
+		tiny: false
+	},
+	genres: {
+		fantasy: false,
+		medievalFantasy: false,
+		historicalFiction: false,
+		western: false,
+		samurai: false,
+		horror: false,
+		scifi: false,
+		spacefaring: false
+	},
+	content: {
+		properName: false,
+		modern: false,
+		religionAndMyths: false,
+		christianity: false,
+		greekRomanMyth: false,
+		sexual: false,
+		illicitSubstances: false,
+		alcohol: false,
+		tobacco: false
+	},
+	person: {
+		realPerson: false
+	},
+	event: {
+		nonPunctualEvent: false
+	},
+	triggers: {
+		humanDeath: false,
+		humanDistress: false,
+		animalDeath: false,
+		animalDistress: false	
+	}
 };
 
 //
@@ -47,6 +165,18 @@ export const blankAppState: StateObject = {
 // SUB-REDUCERS
 //
 //
+const reduceAll = (state: StateObject) => {
+	return {
+		theme: state.theme,
+		settings: {...state.settings},
+		locales: {...state.locales},
+		genres: {...state.genres},
+		content: {...state.content},
+		person: {...state.person},
+		event: {...state.event},
+		triggers: {...state.triggers}
+	};
+}
 
 //
 //
@@ -55,15 +185,48 @@ export const blankAppState: StateObject = {
 //
 export function reducer(state: StateObject = blankAppState, action: ReducerAction) {
 	const payload = action.payload;
-	let final: StateObject = {...state};
+	let final: StateObject;
+	let b: string;
 	switch(action.type) {
 		case UPDATE_THEME:
+			final = reduceAll(state);
 			final.theme = payload;
 			maybeUpdateTheme(state.theme, payload);
 			break;
-		case TOGGLE_BOOL:
-			let b = (payload as keyof StateObject) as boolProp;
-			final[b] = !final[b];
+		case TOGGLE_SETTING:
+			b = payload;
+			final = reduceAll(state);
+			final.settings[b as keyof SettingsObject] = !final.settings[b as keyof SettingsObject];
+			break;
+		case TOGGLE_LOCALE:
+			b = payload;
+			final = reduceAll(state);
+			final.locales[b as keyof LocalesObject] = !final.locales[b as keyof LocalesObject];
+			break;
+		case TOGGLE_GENRE:
+			b = payload;
+			final = reduceAll(state);
+			final.genres[b as keyof GenresObject] = !final.genres[b as keyof GenresObject];
+			break;
+		case TOGGLE_CONTENT:
+			b = payload;
+			final = reduceAll(state);
+			final.content[b as keyof ContentObject] = !final.content[b as keyof ContentObject];
+			break;
+		case TOGGLE_PERSON:
+			b = payload;
+			final = reduceAll(state);
+			final.person[b as keyof PersonObject] = !final.person[b as keyof PersonObject];
+			break;
+		case TOGGLE_EVENT:
+			b = payload;
+			final = reduceAll(state);
+			final.event[b as keyof EventObject] = !final.event[b as keyof EventObject];
+			break;
+		case TOGGLE_TRIGGER:
+			b = payload;
+			final = reduceAll(state);
+			final.triggers[b as keyof TriggersObject] = !final.triggers[b as keyof TriggersObject];
 			break;
 		default:
 			return state;
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -10,15 +10,33 @@ import {
 	IonList,
 	IonItem,
 	IonLabel,
-	IonToggle
+	IonToggle,
+	IonListHeader,
+	IonNote
 } from '@ionic/react';
 import { shallowEqual, useSelector, useDispatch } from "react-redux";
-import { toogleBooleanOption } from '../components/ReduxDucks';
+import { toggleSetting, toggleLocale, toggleGenre, toggleContent, togglePerson, toggleEvent, toggleTrigger } from '../components/ReduxDucks';
 import './Home.css';
 
 const Home = () => {
 	const dispatch = useDispatch();
-	const state = useSelector((state: any) => state, shallowEqual);
+	const [
+		settings,
+		locales,
+		genres,
+		content,
+		person,
+		event,
+		triggers
+	] = useSelector((state: any) => [
+		state.settings,
+		state.locales,
+		state.genres,
+		state.content,
+		state.person,
+		state.event,
+		state.triggers
+	], shallowEqual);
 	return (
 		<IonPage>
 			<IonHeader>
@@ -29,29 +47,146 @@ const Home = () => {
 					</IonButtons>
 				</IonToolbar>
 			</IonHeader>
-			<IonContent fullscreen>
-				<IonList lines="none" className="buttonFilled">
-					<IonItem button={true} onClick={() => dispatch(toogleBooleanOption("shake"))}>
+			<IonContent fullscreen className="onlyList">
+				<IonList lines="none" className="buttonFilled longLabels">
+					<IonListHeader>
+						<IonLabel>General Settings</IonLabel>
+					</IonListHeader>
+					<IonItem button={true} onClick={() => dispatch(toggleSetting("shake"))}>
 						<IonLabel>Shake for new idea</IonLabel>
-						<IonToggle slot="end" checked={state.shake}  />
+						<IonToggle slot="end" checked={settings.shake} />
 					</IonItem>
-					<IonItem>
-						<IonLabel></IonLabel>
+					<IonListHeader>
+						<IonLabel>Locales</IonLabel>
+						<IonNote>Do not suggest locations of the following types</IonNote>
+					</IonListHeader>
+					<IonItem button={true} onClick={() => dispatch(toggleLocale("specific"))}>
+						<IonLabel>Specific places (Notre Dame, the UN, Texas, etc)</IonLabel>
+						<IonToggle slot="end" checked={locales.specific} />
 					</IonItem>
-					<IonItem>
-						<IonLabel></IonLabel>
+					<IonItem button={true} onClick={() => dispatch(toggleLocale("political"))}>
+						<IonLabel>Countries, states and cities (Japan, California, Paris, etc)</IonLabel>
+						<IonToggle slot="end" checked={locales.political} />
 					</IonItem>
-					<IonItem>
-						<IonLabel></IonLabel>
+					<IonItem button={true} onClick={() => dispatch(toggleLocale("large"))}>
+						<IonLabel>Large locations (India, the Amazon basic, etc)</IonLabel>
+						<IonToggle slot="end" checked={locales.large} />
 					</IonItem>
-					<IonItem>
-						<IonLabel></IonLabel>
+					<IonItem button={true} onClick={() => dispatch(toggleLocale("medium"))}>
+						<IonLabel>Medium-sized locations (Jamaica, Siberia, etc)</IonLabel>
+						<IonToggle slot="end" checked={locales.medium} />
 					</IonItem>
-					<IonItem>
-						<IonLabel></IonLabel>
+					<IonItem button={true} onClick={() => dispatch(toggleLocale("small"))}>
+						<IonLabel>Small locations (Mt. Everest, the Vatican, etc)</IonLabel>
+						<IonToggle slot="end" checked={locales.small} />
 					</IonItem>
-					<IonItem>
-						<IonLabel></IonLabel>
+					<IonItem button={true} onClick={() => dispatch(toggleLocale("tiny"))}>
+						<IonLabel>Tiny locations (a bedroom, a wedding, etc)</IonLabel>
+						<IonToggle slot="end" checked={locales.tiny} />
+					</IonItem>
+					<IonListHeader>
+						<IonLabel>Genres</IonLabel>
+						<IonNote>Do not suggest ideas closely associated with any of the following</IonNote>
+					</IonListHeader>
+					<IonItem button={true} onClick={() => dispatch(toggleGenre("fantasy"))}>
+						<IonLabel>Fantasy</IonLabel>
+						<IonToggle slot="end" checked={genres.fantasy} />
+					</IonItem>
+					<IonItem button={true} onClick={() => dispatch(toggleGenre("medievalFantasy"))}>
+						<IonLabel className="subTopic">Western Medieval Fantasy</IonLabel>
+						<IonToggle slot="end" disabled={genres.fantasy} checked={genres.fantasy || genres.medievalFantasy} />
+					</IonItem>
+					<IonItem button={true} onClick={() => dispatch(toggleGenre("historicalFiction"))}>
+						<IonLabel>Historical Fiction</IonLabel>
+						<IonToggle slot="end" checked={genres.historicalFiction} />
+					</IonItem>
+					<IonItem button={true} onClick={() => dispatch(toggleGenre("western"))}>
+						<IonLabel className="subTopic">Westerns</IonLabel>
+						<IonToggle slot="end" disabled={genres.historicalFiction} checked={genres.historicalFiction || genres.western} />
+					</IonItem>
+					<IonItem button={true} onClick={() => dispatch(toggleGenre("samurai"))}>
+						<IonLabel className="subTopic">Medieval Japan</IonLabel>
+						<IonToggle slot="end" disabled={genres.historicalFiction} checked={genres.historicalFiction || genres.samurai} />
+					</IonItem>
+					<IonItem button={true} onClick={() => dispatch(toggleGenre("horror"))}>
+						<IonLabel>Horror</IonLabel>
+						<IonToggle slot="end" checked={genres.horror} />
+					</IonItem>
+					<IonItem button={true} onClick={() => dispatch(toggleGenre("scifi"))}>
+						<IonLabel>Science Fiction</IonLabel>
+						<IonToggle slot="end" checked={genres.scifi} />
+					</IonItem>
+					<IonItem button={true} onClick={() => dispatch(toggleGenre("spacefaring"))}>
+						<IonLabel className="subTopic">Spacefaring Sagas</IonLabel>
+						<IonToggle slot="end" disabled={genres.scifi} checked={genres.scifi || genres.spacefaring} />
+					</IonItem>
+					<IonListHeader>
+						<IonLabel>General Content</IonLabel>
+						<IonNote>Do not suggest ideas containing the following</IonNote>
+					</IonListHeader>
+					<IonItem button={true} onClick={() => dispatch(toggleContent("properName"))}>
+						<IonLabel>Anything with a proper name (George Washington, Twinkies, Japan, etc)</IonLabel>
+						<IonToggle slot="end" checked={content.properName} />
+					</IonItem>
+					<IonItem button={true} onClick={() => dispatch(toggleContent("modern"))}>
+						<IonLabel>Modern ideas (things that occured only in the last 50-70 years)</IonLabel>
+						<IonToggle slot="end" checked={content.modern} />
+					</IonItem>
+					<IonItem button={true} onClick={() => dispatch(toggleContent("religionAndMyths"))}>
+						<IonLabel>Religion, Myths and similar topics</IonLabel>
+						<IonToggle slot="end" checked={content.religionAndMyths} />
+					</IonItem>
+					<IonItem button={true} onClick={() => dispatch(toggleContent("christianity"))}>
+						<IonLabel className="subTopic">Christianity</IonLabel>
+						<IonToggle slot="end" disabled={content.religionAndMyths} checked={content.religionAndMyths || content.christianity} />
+					</IonItem>
+					<IonItem button={true} onClick={() => dispatch(toggleContent("greekRomanMyth"))}>
+						<IonLabel className="subTopic">Greek/Roman Mythology</IonLabel>
+						<IonToggle slot="end" disabled={content.religionAndMyths} checked={content.religionAndMyths || content.greekRomanMyth} />
+					</IonItem>
+					<IonItem button={true} onClick={() => dispatch(toggleContent("sexual"))}>
+						<IonLabel>Sexually suggestive material</IonLabel>
+						<IonToggle slot="end" checked={content.sexual} />
+					</IonItem>
+					<IonItem button={true} onClick={() => dispatch(toggleContent("illicitSubstances"))}>
+						<IonLabel>Illicit Substances (marijuana, cocaine, etc)</IonLabel>
+						<IonToggle slot="end" checked={content.illicitSubstances} />
+					</IonItem>
+					<IonItem button={true} onClick={() => dispatch(toggleContent("alcohol"))}>
+						<IonLabel>Alcohol use</IonLabel>
+						<IonToggle slot="end" checked={content.alcohol} />
+					</IonItem>
+					<IonItem button={true} onClick={() => dispatch(toggleContent("tobacco"))}>
+						<IonLabel>Tobacco Use</IonLabel>
+						<IonToggle slot="end" checked={content.tobacco} />
+					</IonItem>
+					<IonItem button={true} onClick={() => dispatch(togglePerson("realPerson"))}>
+						<IonLabel>Real people</IonLabel>
+						<IonToggle slot="end" checked={person.realPerson} />
+					</IonItem>
+					<IonItem button={true} onClick={() => dispatch(toggleEvent("nonPunctualEvent"))}>
+						<IonLabel>Events that generally last longer than an hour</IonLabel>
+						<IonToggle slot="end" checked={event.nonPunctualEvent} />
+					</IonItem>
+					<IonListHeader>
+						<IonLabel>Trigger Warnings</IonLabel>
+						<IonNote>Do not show ideas even somewhat related to the following</IonNote>
+					</IonListHeader>
+					<IonItem button={true} onClick={() => dispatch(toggleTrigger("humanDeath"))}>
+						<IonLabel>Human death</IonLabel>
+						<IonToggle slot="end" checked={triggers.humanDeath} />
+					</IonItem>
+					<IonItem button={true} onClick={() => dispatch(toggleTrigger("humanDistress"))}>
+						<IonLabel>Human distress</IonLabel>
+						<IonToggle slot="end" checked={triggers.humanDistress} />
+					</IonItem>
+					<IonItem button={true} onClick={() => dispatch(toggleTrigger("animalDeath"))}>
+						<IonLabel>Animal death</IonLabel>
+						<IonToggle slot="end" checked={triggers.animalDeath} />
+					</IonItem>
+					<IonItem button={true} onClick={() => dispatch(toggleTrigger("animalDistress"))}>
+						<IonLabel>Animal distress</IonLabel>
+						<IonToggle slot="end" checked={triggers.animalDistress} />
 					</IonItem>
 				</IonList>
 			</IonContent>
